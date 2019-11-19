53fe6907674d9abbec34f8d883e1cf51696825b2
Mustafa Abozaina <MAbozaina@trudoc24x7.com>
2019-11-19 11:53:44 +0200

    Initial Commit

---
+++ ConsoleProblemSolving/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ConsoleProblemSolving
+//
+//  Created by Mustafa Abozaina on 11/19/19.
+//  Copyright © 2019 Mustafa Abozaina. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
